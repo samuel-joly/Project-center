6095a75d45559d6d72a480f6ce64baf8cd895d53
samuel-joly <samuel-joly@laplateforme.io>
2020-06-09 17:01:15 +0200

    Added search function

--- index.php
+++ index.php
@@ -1,24 +1,35 @@
+<?php 
+	require('classes.php');
+	$user = new userpdo; 
+?>
 <!DOCTYPE html>
 <html lang="fr">
-<?php require('classes.php');
-    $user = new userpdo; ?>
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src='script.js'>
-    </script>
-</head>
-<body>
-<header>
-</header>
-    <main>
-    <input id="search" type="search" name="research">
-    <?php $user->search() ?>
+	<head>
+	    <meta charset="UTF-8">
+	    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+	    <link rel="stylesheet" type="text/css" href="style.css">
+	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	</head>
+	<body>
+		<header>
+		</header>
 
-    </main>
-    <footer></footer>
-</body>
+	    <main class='d-flex justify-content-start'>
+		    <div class='input-group m-2 w-25'>
+			    <input id="search" type="text" name="research" placeholder='Search a name' class='form-control'/>
+			    <div class='input-group-append'>
+				<button class='btn btn-primary' type='button' name='search' id='searchBtn'>Search</button>
+			    </div>
+		    </div>
+		    
+		    <div class='d-flex text-red' id='error_displayer'>
+	
+		    </div>
+
+	    </main>
+
+	    <footer></footer>
+	    <script src='script.js?v=1'></script>
+	</body>
 
 </html>
--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-$("#search").keypress(function() {
-$infosearc = document.getElementById('search').value
-console.log($infosearc)
+$("#search").keyup(function() {
+	value = $("#search").val();
+	if(value.length >= 1 ) {
+		$.ajax({
+			url:"search_controller.php",
+			type:'get',
+			data:{"action":"search" , "value":value},
+			success:function(data){
+				console.log(data);
+				//data = JSON.parse(data);
+				//console.log(data);
+			}	
+		});
+	}
+
 })
