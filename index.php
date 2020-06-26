4e71b8c28915460e209ca0956140342fb990ea2d
samuel-joly <samuel.joly@laplateforme.io>
2020-06-26 12:39:01 +0200

    Added some decoration and secon search bar

--- index.php
+++ index.php
@@ -7,18 +7,54 @@
 	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	</head>
 	<body class='m-0 p-0'>
-	    <header class='d-flex row w-100 m-0 justify-content-center bg-secondary border-secondary'>
+	    <header class='d-flex row w-100 m-0 justify-content-center bg-light border-secondary'>
 
-		<div class='input-group w-100 row col-12 mt-2 justify-content-end p-0 mb-2 mr-2' >
-			<div class='p-0 m-0 row col-lg-3 col-md-5 col-6' class='search_input' id='search_parent'>
-				<input id="search" type="text" name="research" placeholder='Search a name' class='form-control col-9'/>
-				<div class='input-group-append col-3 p-0'>
-					<button class='btn btn-primary col-12' type='button' name='search' id='searchBtn'>Search</button>
+		<div class='input-group w-100 row col-12 mt-2 justify-content-between p-0 mb-2 mr-2' >
+			<div class='col-4 row align-items-center m-0 p-0'>
+				<img src='medias/logo.png' class='col-lg-2 col-4' height='50' />
+				<p class='h3 text-black' class='col-lg-8 col-8 m-0 p-0'>La Plateforme_</p>
+			</div>
+			<div class='p-0 m-0 row col-lg-3 col-md-5 col-6 search_parent align-items-center' >
+				<input id="search" type="text" name="research" placeholder="Cherchez quelqu'un" class='form-control col-lg-9 col-8'/>
+				<div class='input-group-append col-lg-3 col-4 p-0'>
+					<button class='btn btn-primary col-12' style='height:fit-content;'type='button' name='search' id='searchBtn'>Search</button>
 				</div>
 			</div>
 		</div>
 
 	    </header>
+
+	    <main>
+		<div class='w-100 row m-0 p-0' style='background:url("medias/promo2002-dark.jpeg") no-repeat center;background-size:cover; height:50em;'>
+			<div class='input-group row col-12 mt-2 justify-content-center p-0 mb-2 mr-2 align-items-center' >
+				<div class='p-0 m-0 row col-lg-12 align-items-center justify-content-center search_main_parent' style='height:fit-content;'>
+					<p class='text-white text-center col-12 m-0' style='font-size:70px;'>Trouvez un partenaire</p>
+					<p class='text-white text-center col-12 m-0' style='font-size:30px;'>Gérez vos projets</p>
+					<input id="search_main" type="text" name="research" placeholder="Cherchez quelqu'un" class='form-control col-3'/>
+					<div class='input-group-append col-1 p-0'>
+						<button class='btn btn-primary col-12' style='height:fit-content;'type='button' name='search' id='searchBtn'>Search</button>
+					</div>
+				</div>
+			</div>
+			
+			<div class='row col-12 m-0 p-0 justify-content-around'>
+				<div class='col-4 m-0 p-0 row justify-content-center' style='height:fit-content;'>
+					<p class='h3 text-white text-center col-12' style='height:fit-content;'>Trouvez vos projets</p>
+					<img src='medias/project.png' style='height:15em;'/>
+				</div>
+				<div class='col-4 m-0 p-0 row justify-content-center' style='height:fit-content;'>
+					<p class='h3 text-white text-center col-12' style='height:fit-content;'>Cherchez une équipe</p>
+					<img src='medias/collaborate.png' style='height:15em;'/>
+				</div>
+				<div class='col-4 m-0 p-0 row justify-content-center' style='height:fit-content;'>
+					<p class='h3 text-white text-center col-12' style='height:fit-content;'>Codez !</p>
+					<img src='medias/program.png' style='height:15em;'/>
+				</div>
+
+			</div>
+
+		</div>
+	    </main>
 
 	    <footer></footer>
 	    <script src='js/script.js'></script>
--- js/script.js
+++ js/script.js
@@ -1,28 +1,35 @@
-function test() {
-	
+function autocomplete(value, main=true) {
+    if(value.length >= 1 ) {
+        $.ajax({
+		url:"models/search/search_controller.php",
+		type:'get',
+		data:{"action":"search" , "value":value},
+
+		success:function(data){
+			$(".search_list").remove();
+			if(main){
+				$(".search_main_parent").append("<div class='search_list col-4 offset-4 p-0'>"+data+"</div>");
+			} else {
+				$(".search_parent").append("<div class='search_list p-0'>"+data+"</div>");
+			}
+		}
+	});
+    } else {
+	$("#search_list").remove();
+    }
 }
 
 
 
 $("#search").keyup(function() {
-    value = $("#search").val();
-    if(value.length >= 1 ) {
-
-        $.ajax({
-            url:"models/search/search_controller.php",
-            type:'get',
-            data:{"action":"search" , "value":value},
-
-            success:function(data){
-	    	$("#search_list").remove();
-		$("#search_parent").append("<div id='search_list' class='p-0'>"+data+"</div>");
-            }	});
-
-    } else {
-	$("#search_list").remove();
-    }
-
+	value = $("#search").val();
+	autocomplete(value, false);
 })
 
-$("body").click(function() {$("#search_list").remove();})
+$("#search_main").keyup(function() {
+	value = $("#search_main").val();
+	autocomplete(value);
+})
+
+$("body").click(function() {$(".search_list").remove();})
 
