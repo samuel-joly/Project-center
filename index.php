62ca2886785cb6e2713af8c83af442a36f82f228
samuel-joly <samuel.joly@laplateforme.io>
2020-06-27 11:11:01 +0200

    merge prepare

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
 	    </header>
 
 	    <main>
-		<div class='w-100 row m-0 p-0' style='background:url("medias/promo2002-dark.jpeg") no-repeat center;background-size:cover; height:50em;'>
+		<div class='w-100 row  ml-0 mb-3 p-0' style='background:url("medias/promo2002-dark.jpeg") no-repeat center;background-size:cover; height:50em;'>
 			<div class='input-group row col-12 mt-2 justify-content-center p-0 mb-2 mr-2 align-items-center' >
 				<div class='p-0 m-0 row col-lg-12 align-items-center justify-content-center search_main_parent' style='height:fit-content;'>
 					<p class='text-white text-center col-12 m-0' style='font-size:70px;'>Trouvez un partenaire</p>
@@ -52,8 +52,104 @@
 				</div>
 
 			</div>
+		</div>
+
+	
+		<div class='m-1 mt-3 p-1 row justify-content-center'>
+			<div class='card bg-warning border rounded col-5 p-2 m-4' style='height:fit-content;'> 
+				<table class='table col-12 table-stripped table-bordered table-sm mb-0'>
+					<thead class='thead-dark'>
+						<tr>
+							<th class='text-center' style='width:5em;'>Nom</th>
+							<th class='text-center' style='width:7em;'>Taille équipe</th>
+							<th class='text-center' style='width:7em;'>Temps impartis</th>
+							<th class='text-center' style='width:1em;'>PDF</th>
+							<th class='text-center' style='width:8em;'>Groupe</th>
+						</tr>
+					</thead>
+
+
+					<tbody class='bg-light'>
+						<tr style='font-size:1.2em;'>
+							<td class='text-center'>Autocompletion</td><td class='text-center'>2</td><td class='text-center'>4</td>
+							<td class='d-flex justify-content-center'><button class='btn btn-info project_pdf_btn m-0 pb-0 pt-0' style='height:1.8em;font-size:0.8em;' id='pdf_1'>Télécharger</button></td>
+							<td class='text-center '><button class='btn btn-primary project_info_btn m-0 pb-0 pt-0' style='height:1.8em;font-size:0.8em;' id='info_1'>S'inscrire</button></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+
+			<div class='card bg-danger border rounded col-5 p-2 m-4' style='height:fit-content;'> 
+				<table class='table col-12 table-stripped table-bordered table-sm mb-0'>
+					<thead class='thead-dark'>
+						<tr>
+							<th class='text-center' style='width:5em;'>Nom</th>
+							<th class='text-center' style='width:7em;'>Taille équipe</th>
+							<th class='text-center' style='width:7em;'>Temps impartis</th>
+							<th class='text-center' style='width:1em;'>PDF</th>
+							<th class='text-center' style='width:8em;'>Groupe</th>
+						</tr>
+					</thead>
+
+
+					<tbody class='bg-light'>
+						<tr style='font-size:1.2em;'>
+							<td class='text-center'>Autocompletion</td><td class='text-center'>2</td><td class='text-center'>4</td>
+							<td class='d-flex justify-content-center'><button class='btn btn-info project_pdf_btn m-0 pb-0 pt-0' style='height:1.8em;font-size:0.8em;' id='pdf_1'>Télécharger</button></td>
+							<td class='text-center '><button class='btn btn-primary project_info_btn m-0 pb-0 pt-0' style='height:1.8em;font-size:0.8em;' id='info_1'>S'inscrire</button></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+
+			<div class='card bg-success border rounded col-5 p-2 m-4' style='height:fit-content;'> 
+				<table class='table col-12 table-stripped table-bordered table-sm mb-0'>
+					<thead class='thead-dark'>
+						<tr>
+							<th class='text-center' style='width:5em;'>Nom</th>
+							<th class='text-center' style='width:7em;'>Taille équipe</th>
+							<th class='text-center' style='width:7em;'>Temps impartis</th>
+							<th class='text-center' style='width:1em;'>PDF</th>
+							<th class='text-center' style='width:8em;'>Groupe</th>
+						</tr>
+					</thead>
+
+
+					<tbody class='bg-light'>
+						<tr style='font-size:1.2em;'>
+							<td class='text-center'>Autocompletion</td><td class='text-center'>2</td><td class='text-center'>4</td>
+							<td class='d-flex justify-content-center'><button class='btn btn-info project_pdf_btn m-0 pb-0 pt-0' style='height:1.8em;font-size:0.8em;' id='pdf_1'>Télécharger</button></td>
+							<td class='text-center '><button class='btn btn-primary project_info_btn m-0 pb-0 pt-0' style='height:1.8em;font-size:0.8em;' id='info_1'>S'inscrire</button></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+
+			<div class='card border rounded col-5 m-4 p-2' style='height:fit-content;background:#2ebec5;'> 
+				<table class='table col-12 table-stripped table-bordered table-sm mb-0'>
+					<thead class='thead-dark'>
+						<tr>
+							<th class='text-center' style='width:5em;'>Nom</th>
+							<th class='text-center' style='width:7em;'>Taille équipe</th>
+							<th class='text-center' style='width:7em;'>Temps impartis</th>
+							<th class='text-center' style='width:1em;'>PDF</th>
+							<th class='text-center' style='width:8em;'>Groupe</th>
+						</tr>
+					</thead>
+
+
+					<tbody class='bg-light'>
+						<tr style='font-size:1.2em;'>
+							<td class='text-center'>Autocompletion</td><td class='text-center'>2</td><td class='text-center'>4</td>
+							<td class='d-flex justify-content-center'><button class='btn btn-info project_pdf_btn m-0 pb-0 pt-0' style='height:1.8em;font-size:0.8em;' id='pdf_1'>Télécharger</button></td>
+							<td class='text-center '><button class='btn btn-primary project_info_btn m-0 pb-0 pt-0' style='height:1.8em;font-size:0.8em;' id='info_1'>S'inscrire</button></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
 
 		</div>
+
 	    </main>
 
 	    <footer></footer>
